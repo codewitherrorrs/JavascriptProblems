9d19dcb25b07f387a5f1846d606173a5f3205a9d
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-28 13:02:21 +0530

    Add : Find 2 Largest And Smallest

--- program.js
+++ program.js
@@ -20,3 +20,18 @@ else
    console.log("The Second largest element in the array is: "+ second);
 }
 console.log("-------------------------------------------------------");  
+
+//2.Extend the above program to sort the array and then find the 2nd largest and the 2nd smallest element
+let values1=10;
+let array=new Array();
+for(let i=0; i<values1; i++)
+{
+   array[i]=Math.floor(Math.random()*900)+100;
+}
+console.log("\n\nTen three digit random values before sort are:\n-------------------------------------\n" + array);
+//sort value in ascending order
+array.sort();
+console.log("Ten three digit random values after sort are:\n---------------------------------------\n" + array);
+console.log("second largest value: " + array[8]);
+console.log("second smallest value: " + array[1]);
+console.log("-------------------------------------------------------");
