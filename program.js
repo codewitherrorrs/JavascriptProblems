d3925aac766b44ce0a0e3b9aefa30f1c13b2d782
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-28 12:21:00 +0530

    Add : Read Number And Print Word

--- program.js
+++ program.js
@@ -1,44 +1,36 @@
-//Selection Practice Problems with if else if and else
- //1.Read a single digit number and write the number in word 
- let num=Math.floor(Math.random() * 10);
- if(num==0)
- {
-    console.log("zero");
- }
- else if(num==1)
- {
-    console.log("One");
- }
- else if(num==2)
- {
-    console.log("Two");
- }
- else if(num==3)
- {
-    console.log("Three");
- }
- else if(num==4)
- {
-    console.log("Four");
- }
- else if(num==5)
- {
-    console.log("five");
- }
- else if(num==6)
- {
-    console.log("six");
- }
- else if(num==7)
- {
-    console.log("seven");
- }
- else if(num==8)
- {
-    console.log("eight");
- }
- else if(num==9)
- {
-    console.log("nine");
- }  
- console.log("--------------------------------------------"); 
+//2.Read a Number and Display the week day (Sunday, Monday,…)
+let dayNumber=Math.floor(Math.random() * 7)+1;
+
+if(dayNumber==1)
+{
+   console.log("Sunday");
+}
+else if(dayNumber==2)
+{
+   console.log("Monday");
+}
+else if(dayNumber==3)
+{
+   console.log("Tuesday");
+}
+else if(dayNumber==4)
+{
+   console.log("Wednesday");
+}
+else if(dayNumber==5)
+{
+   console.log("Thrusday");
+}
+else if(dayNumber==6)
+{
+   console.log("Friday");
+}
+else if(dayNumber==7)
+{
+   console.log("Saturday");
+}
+else
+{
+   console.log("Please enter number between 1 to 7 because number of week day are 7");
+} 
+console.log("--------------------------------------------"); 
