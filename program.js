94973bc0410a6385b33e4a5fb3cf9c5bdec9dec7
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-28 11:59:16 +0530

    Add : Get Random Dice Value

--- program.js
+++ program.js
@@ -1,5 +1,4 @@
 
-//Sequence Practice Problems
-//UC1- Get random Single digit 
-let result=Math.floor(Math.random() * 10);
-console.log(result);
+//UC2- Use Random to get Dice Number between 1 to 6
+let dice=Math.floor(Math.random() * 6)+1;
+console.log(dice); 
