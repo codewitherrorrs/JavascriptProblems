1971b6beb4cee420a212cbf455d7562385de914b
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-28 12:32:18 +0530

    Add : Power of Two

--- program.js
+++ program.js
@@ -1,40 +1,12 @@
-//3.Read a Number 1, 10, 100, 1000, etc and display unit, ten, hundred,...
 
-let no = Math.floor(Math.random()*1050);
-if(no >=0 && no <=9)
-{
-   option=1;
-}
-else if(no >=10 && no <=99)
-{
-   option=2;
-}
-else if(no >=100 && no <=999)
-{
-   option=3;
-}
-else
-{
-   option=4;
-}
-switch(option)
-{
-    case 1:
-            console.log("Generate random number : "+ no);
-            console.log("Unit");
-            break;
-    case 2:
-            console.log("Generate random number : "+ no);
-            console.log("Ten");
-            break;
-    case 3:
-            console.log("Generate random number : "+ no);
-            console.log("Hundred");
-            break;
-    case 4:
-            console.log("Generate random number : "+ no);
-            console.log("Thousand");
-            break;
-} 
+//Repetition Practice Problems with for loop
+//1. Prints a table of the powers of 2
 
+let value=Math.floor(Math.random()*10);
+let i;
+for (i=0;i<=value;i++)
+   {
+    let power=Math.pow(2,i);
+    console.log("power of 2^"+ i +" is " +power);
+   }
 console.log("--------------------------------------------"); 
