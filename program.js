d88e64c0fd8e6c3f7e671c7791b63b664ecae6a5
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-28 12:30:11 +0530

    Add : Read Number And Print Unit Using Switch

--- program.js
+++ program.js
@@ -1,35 +1,40 @@
-//2. Read a Number and Display the week day (Sunday, Monday,…)
+//3.Read a Number 1, 10, 100, 1000, etc and display unit, ten, hundred,...
 
-let dayNum = Math.floor(Math.random()*7)+ 1;
-switch(dayNum)
+let no = Math.floor(Math.random()*1050);
+if(no >=0 && no <=9)
+{
+   option=1;
+}
+else if(no >=10 && no <=99)
+{
+   option=2;
+}
+else if(no >=100 && no <=999)
+{
+   option=3;
+}
+else
+{
+   option=4;
+}
+switch(option)
 {
     case 1:
-            console.log("Generate random number : "+ dayNum);
-            console.log("Day ==> Sunday");
+            console.log("Generate random number : "+ no);
+            console.log("Unit");
             break;
     case 2:
-            console.log("Generate random number : "+ dayNum);
-            console.log("Day ==> Monday");
+            console.log("Generate random number : "+ no);
+            console.log("Ten");
             break;
     case 3:
-            console.log("Generate random number : "+ dayNum);
-            console.log("Day ==> Tuesday");
+            console.log("Generate random number : "+ no);
+            console.log("Hundred");
             break;
     case 4:
-            console.log("Generate random number : "+ dayNum);
-            console.log("Day ==> Wednesday");
-            break;
-    case 5:
-            console.log("Generate random number : "+ dayNum);
-            console.log("Day ==> Thrusday");
-            break;
-    case 6:
-           console.log("Generate random number : "+ dayNum);
-            console.log("Day ==> Friday");
-            break;
-    case 7:
-            console.log("Generate random number : "+ dayNum);
-            console.log("Day ==> Saturday");
+            console.log("Generate random number : "+ no);
+            console.log("Thousand");
             break;
 } 
+
 console.log("--------------------------------------------"); 
