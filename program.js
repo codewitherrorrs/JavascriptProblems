7094c293e93e1b71e05a500f7fb541aae6b3efbd
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-28 12:42:55 +0530

    Add : Find Power of 2 using While loop

--- program.js
+++ program.js
@@ -1,9 +1,11 @@
-//5.Write a program that computes a factorial of a number taken as input. Such as 5 Factorial – 5! = 1 * 2 * 3 * 4 * 5
-let factNumber=5;
-let fact=1;
-for(i=1;i<=factNumber;i++)
-{
-    fact=fact*i;
-}
-console.log("\nFactorial of "+ factNumber+" is ==>  " + fact) 
-console.log("--------------------------------------------"); 
+//Repetition Practice Problems with while loop
+//1.Write a program that takes a command-line argument n and prints a table of the powers of 2 that are less than or equal to 2^n till 256 is reached..
+let powervalue=Math.floor(Math.random()*10);
+let n=0;
+while (n<=powervalue)
+   {
+    let powerValue=Math.pow(2,n);
+    console.log("power of 2^"+ n +" is " +powerValue);
+    n++
+   } 
+console.log("--------------------------------------------");    
