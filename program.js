bd24a32fc138798169e4c23b73c485ba62f3c998
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-28 12:57:51 +0530

    Add : Conversion Selection Using Function

--- program.js
+++ program.js
@@ -1,11 +1,22 @@
-//Repetition Practice Problems with while loop
-//1.Write a program that takes a command-line argument n and prints a table of the powers of 2 that are less than or equal to 2^n till 256 is reached..
-let powervalue=Math.floor(Math.random()*10);
-let n=0;
-while (n<=powervalue)
+//Functions Practice Problems
+//1.Help user find degF or degC based on their Conversion Selection. Use Case Statement and ensure that the inputs are within the Freezing Point (  0 °C / 32 °F ) and the Boiling Point of Water ( 100 °C / 212 °F )
+   //a. degF = (degC * 9/5) + 32
+   //b. degC = (degF – 32) * 5/9
+
+   function findDegF(degC)
    {
-    let powerValue=Math.pow(2,n);
-    console.log("power of 2^"+ n +" is " +powerValue);
-    n++
-   } 
+      degF = (degC * 9/5) + 32;
+      return degF;
+   }
+   let x1=findDegF(100)
+   console.log("Result in degF: " + x1+"F");
+
+   function findDegC(degF)
+   {
+      degC=(degF - 32) * 5/9;
+      return degC;
+   }
+   let y1=findDegC(212)
+   console.log("Result in degF: " + y1+"C"); 
+
 console.log("--------------------------------------------");    
