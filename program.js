3462c9d5c6f81987f3871a5ef9742e4dc84d3ff6
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-28 12:11:04 +0530

    Add : Check Date And Month

--- program.js
+++ program.js
@@ -1,41 +1,12 @@
-
-//--Selection Practice Problems with if & else  
-//1.Write a program that reads 5 Random 3 Digit values and then outputs the minimum and the maximum value
-//--Selection Practice Problems with if & else  
-//1.Write a program that reads 5 Random 3 Digit values and then outputs the minimum and the maximum value
-let number1=Math.floor(Math.random() * 900)+100;
-console.log("first number: "+number1);
-let number2=Math.floor(Math.random() * 900)+100;
-console.log("Second number: "+number2);
-let number3=Math.floor(Math.random() * 900)+100;
-console.log("third number: "+number3);
-let number4=Math.floor(Math.random() * 900)+100;
-console.log("four number: "+number4);
-let number5=Math.floor(Math.random() * 900)+100;
-console.log("five number: "+number5);
-
-if(number1> number2 && number1>number3 && number1> number4 && number1> number5 )
+//2.Write a program that takes day and month from the command line and prints true if day of month is between March 20 and June 20, false otherwise.
+let month=5;
+let date=20;
+if(((month <= 6 && date <= 20) && ((month >= 3 && date <= 20) && (date<31))))
 {
-    console.log("Greater number ==> "+number1);
-}
-else if(number2> number1 && number2> number3 && number2> number4 && number2> number5 )
-{
-    console.log("Greater number ==> "+number2);
-}
-else if(number2> number1 && number2> number3 && number2> number4 && number2> number5  )
-{
-    console.log("Greater number ==> "+number2);
-}
-else if(number3> number1 && number3> number2 && number3> number4 && number3> number5 )
-{
-    console.log("Greater number ==> "+number3);
-}
-else if(number4> number1 && number4> number2 && number4> number3 && number4> number5 )
-{
-    console.log("Greater number ==> "+number4);
+   console.log("True");
 }
 else
 {
-    console.log("Greater number ==> "+number5);
-}
+   console.log("False");
+} 
 console.log("--------------------------------------------"); 
