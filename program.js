1eb3bdf9612946f95b9ed4cca83690518659fdfe
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-28 12:03:33 +0530

    Add : Find SumAnd Average

--- program.js
+++ program.js
@@ -1,7 +1,16 @@
-//UC3- Add two Random Dice Number and Print the Result
-let dice1=Math.floor(Math.random() * 6)+1;
-console.log(dice1);
-let dice2=Math.floor(Math.random() * 6)+1;
-console.log(dice2);
-let sum=dice1 + dice2;
-console.log("Sum of two random dice: " + sum); 
+/UC4-– Write a program that reads 5 Random 2 Digit values ,then find their sum and the average
+let num1=Math.floor(Math.random() * 90)+10;
+let num2=Math.floor(Math.random() * 90)+10;
+let num3=Math.floor(Math.random() * 90)+10;
+let num4=Math.floor(Math.random() * 90)+10;
+let num5=Math.floor(Math.random() * 90)+10;
+let sum1=num1 + num2 + num3 + num4 + num5;
+console.log("First number value ==> "+num1);
+console.log("Second number value ==> "+num2);
+console.log("First number value ==> "+num3);
+console.log("Second number value ==> "+num4);
+console.log("First number value ==> "+num5);
+let average=sum1/5;
+console.log("Sum : " + sum1); 
+console.log("Average: " + average);  
+console.log("--------------------------------------------"); 
