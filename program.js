0246ec7cea5780d72beeccb1db9dd55f0b8e6c47
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-28 12:59:52 +0530

    Add : Find Largest And Smallest Without SOrt

--- program.js
+++ program.js
@@ -1,22 +1,22 @@
-//Functions Practice Problems
-//1.Help user find degF or degC based on their Conversion Selection. Use Case Statement and ensure that the inputs are within the Freezing Point (  0 °C / 32 °F ) and the Boiling Point of Water ( 100 °C / 212 °F )
-   //a. degF = (degC * 9/5) + 32
-   //b. degC = (degF – 32) * 5/9
-
-   function findDegF(degC)
+//without sort find second smallest value 
+for (let i = 0; i < arrayNum.length; ++i)
+ {
+    if (first > arrayNum[i])
+     {
+        second = first;
+        first = arrayNum[i];
+     } 
+    else if (arrayNum[i] < second && arrayNum[i] != first) 
+    {
+        second = arrayNum[i];
+    }
+}
+if (second == first)
    {
-      degF = (degC * 9/5) + 32;
-      return degF;
+      console.log("There is no second smallest element\n");
    }
-   let x1=findDegF(100)
-   console.log("Result in degF: " + x1+"F");
-
-   function findDegC(degF)
-   {
-      degC=(degF - 32) * 5/9;
-      return degC;
-   }
-   let y1=findDegC(212)
-   console.log("Result in degF: " + y1+"C"); 
-
-console.log("--------------------------------------------");    
+else
+{
+   console.log("The Second largest element in the array is: "+ second);
+}
+console.log("-------------------------------------------------------");  
