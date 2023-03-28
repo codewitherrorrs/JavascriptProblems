4b16ca6f0ebd675ed2fb77b6f9ec16fc9c0cf9f9
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-28 12:01:20 +0530

    Add : Add Two Random Dice Number

--- program.js
+++ program.js
@@ -1,4 +1,7 @@
-
-//UC2- Use Random to get Dice Number between 1 to 6
-let dice=Math.floor(Math.random() * 6)+1;
-console.log(dice); 
+//UC3- Add two Random Dice Number and Print the Result
+let dice1=Math.floor(Math.random() * 6)+1;
+console.log(dice1);
+let dice2=Math.floor(Math.random() * 6)+1;
+console.log(dice2);
+let sum=dice1 + dice2;
+console.log("Sum of two random dice: " + sum); 
