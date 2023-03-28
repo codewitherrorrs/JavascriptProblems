0b8b9438843323b926ef5fd8050b45047d95501c
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-28 12:25:01 +0530

    Add : Get Number And Display Word Using Switch Case

--- program.js
+++ program.js
@@ -1,29 +1,46 @@
-//3.Enter 3 Numbers do following arithmetic operation and find the one that is maximum and minimum
- //1. a + b * c        
- let a=10;
- let b=20;
- let c=15;
- let output= a + b * c ;
- console.log("Result1 are : " + output); 
-
- //2. a % b + c 
- let a1=10;
- let b1=20;
- let c1=15;
- let output1= ((a1 % b1) + c1 );
- console.log("Result2 are : " + output1); 
-
- //3. c + a / b 
- let a2=10;
- let b2=20;
- let c2=15;
- let output2= (c2 + (a2 / b2));
- console.log("Result3 are : " + output2); 
-
- //4. a * b + c
- let x=10;
- let y=20;
- let z=15;
- let output3= ((x * y) + z);
- console.log("Result4 are : " + output3); 
- console.log("--------------------------------------------"); 
+//Selection Practice Problems with case statements
+//1. Read a single digit number and write the number in word using Case
+let number = Math.floor(Math.random()*10);
+switch(number)
+{
+    case 0:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> Zero");
+            break;
+    case 1:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> One");
+            break;
+    case 2:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> Two");
+            break;
+    case 3:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> Three");
+            break;
+    case 4:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> Four");
+            break;
+    case 5:
+           console.log("Generate random number : "+ number);
+            console.log("Word ==> Five");
+            break;
+    case 6:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> Six");
+            break;
+    case 7:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> Seven");
+            break;
+    case 8:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> Eight");
+            break;
+    case 9:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> Nine");
+            break;
+} 
